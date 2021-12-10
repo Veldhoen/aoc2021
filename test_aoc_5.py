e8d1417a69c7094356f5fb542cceb08b1ba407cc
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-12-10 10:56:01 +0100

    Create files

---
+++ test_aoc_5.py
@@ -0,0 +1,44 @@
+import aocd
+
+
+def parse(puzzle_input):
+    """Parse input"""
+    lines = puzzle_input.splitlines()
+    return None
+
+def part1(data):
+    return 0
+
+def part2(data):
+    return 0
+
+def solve(day=5):
+
+    """Solve the puzzle for the given input"""
+
+    data = aocd.get_data(day=day)
+    #data = example_data
+
+
+    print('part one:', part1(parse(data)))
+    print('part two:', part2(parse(data)))
+
+example_data = """7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+
+22 13 17 11  0
+ 8  2 23  4 24
+21  9 14 16  7
+ 6 10  3 18  5
+ 1 12 20 15 19
+
+ 3 15  0  2 22
+ 9 18 13 17  5
+19  8  7 25 23
+20 11 10 24  4
+14 21 16 12  6
+
+14 21 17 24  4
+10 16 15  9 19
+18  8 23 26 20
+22 11 13  6  5
+ 2  0 12  3  7"""
