38150b07b72d46f392e0b55a242b5cbc55b6b4fe
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-12-13 19:35:21 +0100

    AoC day 13

--- aoc_13.py
+++ aoc_13.py
@@ -5,21 +5,69 @@ import sys
 
 
 def parse(puzzle_input):
+    lines = puzzle_input.splitlines()
 
+    coordinates = []
+    folds = []
+    for line in lines:
+        try:
+            assert line[0].isnumeric()
+            coordinates.append([int(num) for num in line.split(',')])
+        except IndexError:
+            continue
+        except AssertionError:
+            horizontal = line.split('=')[0][-1]=='y'
+            position = int(line.split('=')[1])
+            folds.append((horizontal,position))
+
+    return turn_to_grid(coordinates), folds
     """Parse input"""
 
+def turn_to_grid(points):
+    max_x = max([coord[0] for coord in points])
+    max_y = max([coord[1] for coord in points])
+
+    grid = [[0 for i in range(max_x+1)] for i in range(max_y+1)]
+    for (x,y) in points:
+        grid[y][x] = 1
+    return grid
+
+def fold_paper(grid, horizontal, position):
+    if horizontal:
+        max = len(grid)
+        for i in range(0,max-position):
+            grid[position-i] = [sum(x) for x in zip(grid[position-i],grid[position+i])]
+        grid = grid[:position]
+        return grid
+    else:
+        max = len(grid[0])
+        for i in range(1,max-position):
+            for j in range(len(grid)):
+                grid[j][position-i] += grid[j][position+i]
+        grid = [line[:position] for line in grid]
+    return grid
+
+def count_the_dots(grid):
+    return sum([sum([int(c>0) for c in line]) for line in grid])
 
 def part1(data):
+    grid, folds = parse(puzzle_input=data)
+    first_fold = fold_paper(grid, folds[0][0], folds[0][1])
+    return count_the_dots(first_fold)
 
     """Solve part 1"""
 
 
 def part2(data):
+    grid, folds = parse(puzzle_input=data)
+    for fold in folds:
+        grid = fold_paper(grid, fold[0], fold[1])
+    print('\n'.join([''.join(['#' if c>0 else '.' for c in line]) for line in grid]))
 
     """Solve part 2"""
 
 
-def solve(day=-1):
+def solve(day=13):
 
     """Solve the puzzle for the given input"""
 
--- test_aoc_13.py
+++ test_aoc_13.py
@@ -1,2 +1,85 @@
+import aoc_13 as target
+
+
 def test_parse():
-    assert False
+    grid, folds = target.parse(puzzle_input=example)
+    assert grid_to_paper(grid) == example_paper
+    assert folds == example_folds
+
+
+def test_count_the_dots():
+    assert target.count_the_dots(target.turn_to_grid(example_dots)) == example_paper.count('#')
+    fold_1 = target.fold_paper(target.turn_to_grid(example_dots), example_folds[0][0], example_folds[0][1])
+    assert target.count_the_dots(fold_1) == 17
+    fold_2 = target.fold_paper(fold_1, example_folds[1][0], example_folds[1][1])
+    assert target.count_the_dots(fold_2) == example_fold_2.count('#')
+
+def test_fold_paper():
+    fold_1 = target.fold_paper(target.turn_to_grid(example_dots),example_folds[0][0],example_folds[0][1])
+    assert grid_to_paper(fold_1) == example_fold_1
+    fold_2 = target.fold_paper(fold_1,example_folds[1][0],example_folds[1][1])
+    assert grid_to_paper(fold_2) == example_fold_2
+
+
+def grid_to_paper(grid):
+    return '\n'.join([''.join(['#' if c>0 else '.' for c in line]) for line in grid])
+
+def test_turn_to_grid():
+    assert grid_to_paper(target.turn_to_grid(example_dots)) == example_paper
+
+example = """6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5"""
+
+example_dots = [(6,10),(0,14),(9,10),(0,3),(10,4),(4,11),(6,0),(6,12),(4,1),(0,13),(10,12),(3,4),(3,0),(8,4),(1,10),(2,14),(8,10),(9,0)]
+example_folds = [(True,7),(False,5)]
+
+example_paper = """...#..#..#.
+....#......
+...........
+#..........
+...#....#.#
+...........
+...........
+...........
+...........
+...........
+.#....#.##.
+....#......
+......#...#
+#..........
+#.#........"""
+
+example_fold_1 = """#.##..#..#.
+#...#......
+......#...#
+#...#......
+.#.#..#.###
+...........
+..........."""
+
+example_fold_2 = """#####
+#...#
+#...#
+#...#
+#####
+.....
+....."""
