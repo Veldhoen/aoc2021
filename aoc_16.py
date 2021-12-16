dc5759063a4999b94bf763f70d096492fb717704
Sara Veldhoen <sara.veldhoen@kb.nl>
2021-12-16 15:25:31 +0100

    Solution and tests for day 16

--- aoc_16.py
+++ aoc_16.py
@@ -1,29 +1,79 @@
-import pathlib
 import aocd
 
-import sys
+def hex_to_bin(hex_rep):
+    dic = {'0': '0000', '1': '0001','2': '0010','3': '0011','4': '0100','5': '0101','6': '0110','7': '0111',
+     '8': '1000', '9': '1001','A': '1010','B': '1011','C': '1100','D': '1101','E': '1110','F': '1111'}
+    return ''.join([dic[c] for c in hex_rep])
+
+def read_header(bin_rep):
+    version = int(bin_rep[:3],2)
+    type_id = int(bin_rep[3:6],2)
+    return version, type_id
 
 
-def parse(puzzle_input):
+def read_literal(bin_rep):
+    groups = [bin_rep[i:i + 5] for i in range(0, len(bin_rep), 5)]
+    to_interpret = ''
+    for group in groups:
+        to_interpret += group[1:]
+        if group[0]=='0':
+            break
+    value = int(to_interpret,2)
+    rest = bin_rep[len(to_interpret)//4*5:]
+    return value, rest
 
-    """Parse input"""
+def read_operator(bin_rep):
+    length_type_id = bin_rep[0]
+    length_of_subpackets = 0
+    number_of_subpackets = 0
+    if length_type_id == '0':
+        length_of_subpackets = int(bin_rep[1:16],2)
+        bin_rep = bin_rep[16:]
+    else:
+        number_of_subpackets = int(bin_rep[1:12],2)
+        bin_rep = bin_rep[12:]
+    version_sum = 0
+    cnt = 0
+    eaten = 0
+    value_list = []
+    while eaten < length_of_subpackets or cnt < number_of_subpackets:
+        next_value, summed_versions,rest = read_packet(bin_rep)
+        eaten += len(bin_rep)-len(rest)
+        value_list.append(next_value)
+        bin_rep = rest
+        version_sum+= summed_versions
+        cnt +=1
+    return value_list, version_sum, bin_rep
 
+def read_packet(bin_rep):
+    if len(bin_rep)<6: return 0, bin_rep
+    version, type_id = read_header(bin_rep[:6])
+    if type_id == 4:
+        value, rest = read_literal(bin_rep[6:])
+        summed_versions = 0
+    else:
+        value_list, summed_versions, rest = read_operator(bin_rep[6:])
+        if type_id == 0: value = sum(value_list)
+        elif type_id == 1: value = functools.reduce((lambda x, y: x*y), value_list)
+        elif type_id == 2: value = min(value_list)
+        elif type_id == 3: value = max(value_list)
+        elif type_id == 5: value =  int(value_list[0] > value_list[1])
+        elif type_id == 6: value =  int(value_list[0] < value_list[1])
+        elif type_id == 7: value =  int(value_list[0] == value_list[1])
+    summed_versions += version
+    return value, summed_versions, rest
 
 def part1(data):
-
-    """Solve part 1"""
-
+    hex = hex_to_bin(data)
+    _, version_sum, _ = read_packet(hex)
+    return version_sum
 
 def part2(data):
+    hex = hex_to_bin(data)
+    value, _, _ = read_packet(hex)
+    return value
 
-    """Solve part 2"""
-
-
-def solve(day=-1):
-
-    """Solve the puzzle for the given input"""
-
+def solve(day=16):
     data = aocd.get_data(day=day)
-
     print('Part one:', part1(data))
     print('Part two:', part2(data))
--- test_aoc_16.py
+++ test_aoc_16.py
@@ -1,2 +1,57 @@
+import aoc_16 as target
+
+
+def test_part1():
+    examples = [
+        ('8A004A801A8002F478', 16),
+        ('620080001611562C8802118E34', 12),
+        ('C0015000016115A2E0802F182340', 23),
+        ('A0016C880162017C3686B18A3D4780', 31)
+    ]
+    for hex, version_sum in examples:
+        assert target.part1(hex) == version_sum
+
+
+def test_hex_to_bin():
+    assert target.hex_to_bin('D2FE28') == '110100101111111000101000'
+
+
+def test_read_literal():
+    literal, unused = target.read_literal('110100101111111000101000'[6:])
+    assert literal == 2021
+    assert unused == '000'
+
+
+def test_read_operator():
+    operator_content = '00000000000110111101000101001010010001001000000000'
+    value, summed_values, rest = target.read_operator(operator_content)
+    assert summed_values == int('110', 2) + int('010', 2)
+    assert rest == '0000000'
+
+    operator_content = '10000000001101010000001100100000100011000001100000'
+    value, summed_values, rest = target.read_operator(operator_content)
+    assert summed_values == int('010', 2) + int('100', 2) + int('001', 2)
+    assert rest == '00000'
+
+
 def test_read_packet():
-    assert False
+    value, sum, rest = target.read_packet('110100101111111000101000')
+    assert sum == 6
+
+    value, sum, rest = target.read_packet('00111000000000000110111101000101001010010001001000000000')
+    assert sum == int('001', 2) + int('110', 2) + int('010', 2)
+
+
+def test_part2():
+    examples = [
+        ('C200B40A82',3),
+        ('04005AC33890',54),
+        ('880086C3E88112', 7),
+        ('CE00C43D881120', 9),
+        ('D8005AC2A8F0',1),
+        ('F600BC2D8F',0),
+        ('9C005AC2F8F0', 0),
+        ('9C0141080250320F1802104A08',1)]
+    for example, value in examples:
+        assert target.part2(example) == value
+
